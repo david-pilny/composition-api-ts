23799f0a0ac1eec21fc2aac8482b96bff867bc1c
david-pilny <93579404+david-pilny@users.noreply.github.com>
2023-04-19 20:30:33 +0200

    First commmit

---
+++ src/components/ControlBar.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+import { defineEmits } from 'vue'
+
+const emit = defineEmits<{
+  (event: 'add-number', num: number): void
+  (event: 'reset-count'): void
+}>()
+</script>
+
+<template>
+  <button @click="emit('add-number', 1)">Add</button>
+  <button @click="emit('reset-count')">Reset</button>
+</template>
